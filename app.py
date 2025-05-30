f46bae0e56b5a4c68ba0ccfbd80984352488d364
Siddharth Soni <siddharth.soni588@gmail.com>
2025-05-30 21:21:30 +0530

    bug fix

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def ask():
     return jsonify({'response': response.choices[0].message.content})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
